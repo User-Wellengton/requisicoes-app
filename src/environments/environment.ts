43d3cc3fca3ca04805e4fb2ac09b42a5ae10f1a8
User-Wellengton <wellengtonjm@gmail.com>
2022-08-28 11:03:46 -0300

    Att Projeto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCuCDwcyX4QGyoWJjLtFkjcjMGm-3Ef2Rk",
+    authDomain: "requisicoes-app-f9bda.firebaseapp.com",
+    projectId: "requisicoes-app-f9bda",
+    storageBucket: "requisicoes-app-f9bda.appspot.com",
+    messagingSenderId: "52204741785",
+    appId: "1:52204741785:web:22061095110943da6a0408"
+  }
+  
 };
 
 /*
