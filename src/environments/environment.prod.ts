6988ffde7c5636cfbbfb9bb854d0e6339dc8f517
User-Wellengton <wellengtonjm@gmail.com>
2022-09-13 16:18:08 -0300

    Att

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  firebase:{
+  firebase: {
     apiKey: "AIzaSyCuCDwcyX4QGyoWJjLtFkjcjMGm-3Ef2Rk",
     authDomain: "requisicoes-app-f9bda.firebaseapp.com",
     projectId: "requisicoes-app-f9bda",
@@ -8,4 +8,5 @@ export const environment = {
     messagingSenderId: "52204741785",
     appId: "1:52204741785:web:22061095110943da6a0408"
   }
+
 };
